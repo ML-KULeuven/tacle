9d1b7a42bfd6a953389179b55a5fb08a3204dab0
sparamonov <sergey.paramonov@cs.kuleuven.be>
2016-03-17 14:24:59 +0100

    fixed a couple of nasty bugs

--- Code/src/generate_asp_code.py
+++ Code/src/generate_asp_code.py
@@ -5,6 +5,7 @@ from pprint import pprint
 groups, tables = get_groups_tables("data/examples.csv", "data/examples.groups")
 #pprint(groups)
 for group in groups:
+# print(group.table.data)
   print(group.get_group_data())
-  break
+# break
 #pprint(tables)
