926008f535007af894338db973d7c85c983a867d
Samuel Kolb <samuel.kolb@me.com>
2016-03-28 11:17:42 +0200

    Reformatted to use spaces

--- Code/src/engine.py
+++ Code/src/engine.py
@@ -4,17 +4,17 @@ from subprocess import Popen, PIPE, STDOUT
 
 
 def run_command(command, input_data=None):
-	if isinstance(input_data, str):
-		input_data = input_data.encode("utf-8")
-	p = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-	data = p.communicate(input=input_data)
+    if isinstance(input_data, str):
+        input_data = input_data.encode("utf-8")
+    p = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    data = p.communicate(input=input_data)
 
-	# noinspection PyUnresolvedReferences
-	return data[0].decode("utf-8")
+    # noinspection PyUnresolvedReferences
+    return data[0].decode("utf-8")
 
 
 def local(filename):
-	return os.path.dirname(os.path.realpath(__file__)) + "/../" + filename
+    return os.path.dirname(os.path.realpath(__file__)) + "/../" + filename
 
 
 class TempFile:
