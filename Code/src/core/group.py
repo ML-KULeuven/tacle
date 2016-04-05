37c1dc7dabeab8c446325e774eefd780a0e66bb4
Samuel Kolb <samuel.kolb@me.com>
2016-04-05 13:41:41 +0200

    Simplify cast code

--- Code/src/core/group.py
+++ Code/src/core/group.py
@@ -111,23 +111,12 @@ class GType(Enum):
     string = 2
 
 
-class EmptyInt(int):
-    pass
-
-
-class EmptyFloat(float):
-    pass
-
-
 def cast(gtype: GType, value):
+    if detect_type(value) == GType.nan.value:
+        return None
     if gtype == GType.int:
-        if detect_type(value) == GType.nan.value:
-            return EmptyInt()
         return int(value)
     elif gtype == GType.float:
-        if detect_type(value) == GType.nan.value:
-            # return EmptyFloat()
-            return None
         match = percent_pattern.match(str(value))
         return float(value) if not match else float(str(value).replace("%", "")) / 100.0
     elif gtype == GType.string:
