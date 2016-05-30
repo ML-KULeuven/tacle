7288329a6aeee3e2c0e12cfa478cb0707ebb3f08
sparamonov <sergey.paramonov@cs.kuleuven.be>
2016-05-30 18:56:36 +0200

    testing post data tranfser

--- tacle/main.py
+++ tacle/main.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('table.html')
+
+@app.route("/handle_constraints",methods=["POST"])
+def handle_constraints():
+    if request.method == 'POST':
+      print(request.files['file'])
+      if 'csv_data' not in request.files or 'tables_json' not in request.files:
+        return "file is missing"
+    else:
+      return redirect("/")
+#TODO
+
+@app.route('/testpost')
+def test_post():
+  return render_template('test.html')
 
 if __name__ == "__main__":
     app.run()
