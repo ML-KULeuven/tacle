7f8256ce514b837db84453fe140841b92231c969
Samuel Kolb <samuel.kolb@me.com>
2018-09-02 12:23:06 +0200

    Fixed casting bug

--- tacle/indexing.py
+++ tacle/indexing.py
@@ -116,7 +116,7 @@ class Typing(object):
         elif cell_type == Typing.currency:
             return float(re.sub(Typing.currency_symbols, "", str(value)))
         elif cell_type in [Typing.int, Typing.float, Typing.numeric]:
-            converted = float(re.sub(Typing.place_holder, "", str(value))) if value not in [None, "#?"] else np.nan
+            converted = float(re.sub(Typing.place_holder, "", str(value))) if value not in [None, "#?", ""] else np.nan
             if cell_type == Typing.int and not np.isnan(converted):
                 converted = int(converted)
             return converted
