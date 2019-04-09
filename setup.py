80bf0ca7982139da750969e6fccce01737bc9236
Samuel Kolb <samuel.kolb@me.com>
2019-04-09 15:36:41 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ URL = 'https://github.com/ML-KULeuven/tacle'
 EMAIL = 'samuel.kolb@me.com'
 AUTHOR = 'Samuel Kolb'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = "0.7.4"
+VERSION = "0.7.5"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
